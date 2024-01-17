e2b6724cb129c4604fae5884a3d0c4226444dd55
yaoguaiba232 <1610759822@qq.com>
2024-01-17 15:18:38 +0800

    Site updated: 2024-01-17 15:18:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/04/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/04/hello-world/","2024/01/17/枯萎的野草-怎配得上栀子花/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
