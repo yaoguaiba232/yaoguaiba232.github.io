d231173a92c7f1291b68d3f93570ca19fa668031
yaoguaiba232 <1610759822@qq.com>
2024-04-24 16:05:38 +0800

    Site updated: 2024-04-24 16:05:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/04/hello-world/","2024/01/25/枫/","2024/01/17/枯萎的野草-怎配得上栀子花/","2024/02/20/知我/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2024/02/20/知我/","2024/04/24/英雄联盟直播/","2023/08/04/hello-world/","2024/01/17/枯萎的野草-怎配得上栀子花/","2024/01/25/枫/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
